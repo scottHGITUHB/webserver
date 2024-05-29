9b63b0c0b83584f0aaba0b004af5e89ba92f17d5
scottHGITUHB <150643328+scottHGITUHB@users.noreply.github.com>
2024-05-29 16:43:18 +0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,9 @@
+salary_list = [
+    {"username": "张三", "salary": 10000, "department": "技术部", "position": "程序员", "password": "admin","level":1},
+    {"username": "李四", "salary": 20000, "department": "技术部", "position": "程序员", "password": "777","level":1},
+    {"username": "王五", "salary": 30000, "department": "技术部", "position": "程序员", "password": "admin","level":1},
+    {"username": "赵六", "salary": 50000, "department": "技术部", "position": "程序员", "password": "admin","level":1},
+    {"username": "孙七", "salary": 60000, "department": "技术部", "position": "程序员", "password": "admin","level":1},
+    {"username": "周八", "salary":  70000, "department": "技术部", "position": "程序员", "password": "admin","level":1},
+    {"username": "admin", "salary": 70000, "department": "技术部", "position": "管理员", "password": "admin","level":2}
+    ]
