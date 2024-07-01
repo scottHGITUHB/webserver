7fb8d6cd4f328d542faa45178dd9ac2dd563c9a1
scottHGITUHB <150643328+scottHGITUHB@users.noreply.github.com>
2024-07-01 21:17:07 +0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,15 @@
+salary_list = [
+    {"username": "张三", "salary": 10000, "department": "技术部", "position": "程序员", "password": "admin","level":1},
+    {"username": "李四", "salary": 20000, "department": "技术部", "position": "程序员", "password": "777","level":1},
+    {"username": "王五", "salary": 30000, "department": "技术部", "position": "程序员", "password": "admin","level":1},
+    {"username": "赵六", "salary": 50000, "department": "技术部", "position": "程序员", "password": "admin","level":1},
+    {"username": "孙七", "salary": 60000, "department": "技术部", "position": "程序员", "password": "admin","level":1},
+    {"username": "周八", "salary":  70000, "department": "技术部", "position": "程序员", "password": "admin","level":1},
+    {"username": "admin", "salary": 70000, "department": "技术部", "position": "管理员", "password": "admin","level":2}
+    ]
+customers = [
+    {"username": "李小明", "organization":"**技术大学", "department": "物流管理专业", "position": "学生", "password": "admin"},
+    {"username": "李四柱", "organization":"**大学", "department": "城市轨道交通专业", "position": "教授", "password": "777"},
+    {"username": "王二狗", "organization": "**大学", "department": "电子技术专业", "position": "专业主任", "password": "admin"},
+    {"username": "1", "organization": "**大学", "department": "电子技术专业", "position": "专业主任", "password": "1"}
+]
